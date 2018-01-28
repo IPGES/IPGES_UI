317d5255eaf25866c768cf58bc56f87649f27a41
Unknown <jiahan.jim.liu@gmail.com>
2018-01-28 17:34:21 -0600

    Submit Button Works
    Lifted Up State and Added Functions.

--- react-backend/client/src/Graph/Temp.jsx
+++ react-backend/client/src/Graph/Temp.jsx
@@ -1,49 +1,13 @@
 import React from 'react';
-import NavigationBar from '../Components/NavigationBar.jsx';
-import NumInput from '../Components/NumInput.jsx';
-import Footer from '../Components/Footer.jsx';
-import Chart from './Chart.jsx';
+import fusioncharts from 'fusioncharts';
+// Load the charts module
+import charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+import TM from 'fusioncharts/themes/fusioncharts.theme.fint';
+
+charts(fusioncharts)
 
 var myDataSource = {
-		chart: {
-		  	caption: "Load Power Consumption",
-        numberSuffix: "W",
-        "yaxisname": "Power",
-        "xaxisname": "Time",
-        "yaxisminValue": "0",
-        "yaxismaxValue": "1400",
-        "forceAxisLimits" : "1",
-        "pixelsPerPoint": "1",
-        "pixelsPerLabel": "24",
-        "lineThickness": "1",
-
-        "compactdatamode" : "1",
-				"dataseparator" : "|",
-
-        "showBorder" : "1",
-        "borderThickness" : "3",
-        "borderColor" : "#cc5500",
-        "borderAlpha" : "100",
-
-        "showCanvasBorder" : "1",
-        "canvasBorderThickness" : "3",
-        "canvasBorderColor" : "#cc5500",
-        "canvasBorderAlpha" : "100",
-
-        "color" : "#100",
-
-		},
-    "categories": [{
-  "category":  "Jan 01|Jan 02|Jan 03|"
-      }],
-      "dataset": [
-				{
-					"data": "978|976|955"
-				}
-      ]
-}
-
-var myDataSource2 = {
 		chart: {
 		  	caption: "Load Power Consumption",
         numberSuffix: "W",
@@ -92,61 +56,20 @@ var staticChartConfigs = {
   	dataSource: myDataSource
 };
 
-var staticChartConfigs2 = {
-	id: "static-chart",
-    renderAt: "static-chart-container",
-	type: "zoomline",
-	width:600,
-    height: 350,
-  	dataFormat: "json",
-  	dataSource: myDataSource2
-};
-
-class GraphPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {users: [],
-                  tab: 0,
-                  childChartConfigs: staticChartConfigs
-                }
-
-    this.handleTabChange = this.handleTabChange.bind(this);
-    this.handleTimeSubmit = this.handleTimeSubmit.bind(this);
-  }
-
-  handleTabChange(tabNum) {
-    this.props.onTabChange(tabNum);
-  }
-
-  handleTimeSubmit(tabNum) {
-    this.setState({
-      childChartConfigs: staticChartConfigs2
-    });
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-  }
+class ZoomChart extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {staticChartConfigs: staticChartConfigs
+		}
+	}
 
   render() {
     return (
       <div>
-        <NavigationBar onTabChange={this.handleTabChange}/>
-        <NumInput onTimeSubmit={this.handleTimeSubmit}/>
-        <Chart chartConfig={this.state.childChartConfigs}/>
-        <Footer />
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
+        <ReactFC {...this.state.staticChartConfigs} />
       </div>
     );
   }
 }
 
-export default GraphPage;
+export default ZoomChart;
