be780d19d1086e10fdda9c610c8e97585ff2ed99
Unknown <jiahan.jim.liu@gmail.com>
2018-02-03 16:21:52 -0600

    mongoDB tutorial

--- react-backend/app.js
+++ react-backend/app.js
@@ -1,29 +1,50 @@
 var express = require('express');
-var path = require('path');
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
+var path = require('path'); //may not need
+var mongoose = require("mongoose");
 
 var index = require('./routes/index');
 var zoomGraphNotes = require('./routes/zoomGraphNotes');
 var individualGraph = require('./routes/individualGraph')
 var totalsGraph = require('./routes/totalsGraph')
 
+var mongoDB = "mongodb://localhost/IPGES";
+mongoose.connect(mongoDB, function (err) {
+  if (err) throw err;
+  console.log("Connected to Mongo!");
+});
+var timeSchema = mongoose.Schema({
+  time: Number,
+  pv: Number,
+  inverter: Number,
+  wind: Number,
+  grid: Number,
+  load: Number
+});
+
+var TimeEntry = mongoose.model('ipgesModel', timeSchema);
+var singleTimeEntry = new TimeEntry ({
+  time: 1200,
+  pv: 1,
+  inverter: 2,
+  wind: 3,
+  grid: 4,
+  load: 5
+});
+singleTimeEntry.save(function(err) {
+  if (err) throw err;
+  console.log('Time entry successfully saved.');
+});
+var dbResults = [];
+TimeEntry.find({
+  time: 1200
+}).exec(function(err, dbResults) {
+  if (err) throw err;
+  console.log(dbResults);
+});
+
+
+
 var app = express();
-
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', index);
 app.use('/zoomGraphNotes', zoomGraphNotes);
@@ -47,13 +68,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//   console.log(req);
-//   //res.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
 
 const port = process.env.PORT || 3500;
 app.listen(port);
